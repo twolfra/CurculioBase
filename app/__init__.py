0f204c1f62bf7c9cec3479ecfc496e7f0af201fe
twolfra <twolfram030@gmail.com>
2025-05-07 09:10:08 +0200

    improved layout

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from .models import db
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, static_folder='../static')
     app.config.from_pyfile('../config.py')
 
     db.init_app(app)

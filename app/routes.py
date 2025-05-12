b9247d9b7dc1bc45169c56ddbca6ff9bfa625ad3
twolfra <twolfram030@gmail.com>
2025-05-12 16:10:31 +0200

    added update button

--- app/routes.py
+++ app/routes.py
@@ -41,3 +41,26 @@ def delete(interaction_id):
     db.session.commit()
     return redirect('/relations')
 
+@main.route('/edit/<int:interaction_id>', methods=['GET', 'POST'])
+def edit(interaction_id):
+    relation = Relation.query.get_or_404(interaction_id)
+
+    if request.method == 'POST':
+        relation.host_species_name = request.form['host']
+        relation.parasite_species_name = request.form['parasite']
+        relation.citation_key = request.form['citation_key']
+        relation.L_parasitic_mode = request.form['L_parasitic_mode']
+        relation.L_host_organ = request.form['L_host_organ']
+        relation.A_parasitic_mode = request.form['A_parasitic_mode']
+        relation.A_host_organ = request.form['A_host_organ']
+        relation.credibility = request.form['credibility']
+        relation.comment = request.form['comment']
+        relation.entry_source = request.form['entry_source']
+        relation.entry_by = request.form['entry_by']
+
+        db.session.commit()
+        return redirect('/relations')
+
+    return render_template('edit_relation.html', relation=relation)
+
+
